a3b5e50358758fe81a967a9a930e3fdce5e0d3de
YESUBZERO <miguel.10hid@gmail.com>
2025-01-28 19:41:47 -0500

    Add: AISMessage struct

--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -9,8 +9,16 @@ import (
 
 // Estructura del mensaje AIS
 type AISMessage struct {
-	MsgType int `json:"msg_type"`
-	IMO     int `json:"imo"`
+	MsgType   int     `json:"msg_type"`
+	IMO       int     `json:"imo"`
+	MMSI      int     `json:"mmsi"`
+	CALLSIGN  string  `json:"callsign"`
+	SHIPNAME  string  `json:"shipname"`
+	SHIP_TYPE string  `json:"ship_type"`
+	STATUS    string  `json:"status"`
+	SPEED     float64 `json:"speed"`
+	LON       float64 `json:"lon"`
+	LAT       float64 `json:"lat"`
 }
 
 // Procesar un mensaje Kafka
